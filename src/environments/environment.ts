e6056bff232c4f13930acda4edb79dbb6600c233
159965658 <159965658@qq.com>
2018-03-29 17:54:53 +0800

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: false, 
   // api: 'http://192.168.1.41:8088/',
-  api: 'http://ijkwebapi.csruntitan.com',
+  api: 'http://ijkwebapi.csruntitan.com',  
   pathImg: 'http://192.168.1.41:8088/',
   //s_domain_name: 'yuming.csruntitan.com',
   s_domain_name: 'hg.csruntitan.com',
